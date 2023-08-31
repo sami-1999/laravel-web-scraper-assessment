a7c3a2625737c56e1d54685ef39651a1e0353c2e
samiuzzamansalsoft5762 <sami.zaman@metafroliclabs.com>
2023-08-31 14:23:01 +0500

    changes in kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('scrape:movies')->everyMinute();
+        // $schedule->command('scrape:movies')->everyMinute();
+        $schedule->command('scrape:movies')->dailyAt('12:00');
+
     }
 
     /**
